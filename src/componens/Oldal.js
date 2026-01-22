a27f834229bb4a4b70765bb015c4dec8949b5bcf
DDorka02 <dobszaydorka@gmail.com>
2026-01-22 15:13:57 +0100

    próbálkozás

---
+++ src/componens/Oldal.js
@@ -0,0 +1,93 @@
+import "./Oldal.css";
+
+const Oldal = () => {
+  return (
+    <div className="App">
+       <article className="article">
+            <div className="emelet">
+              <div className="kepek">
+                <div className="oszlop">
+                <div className="kep1">
+                  <img src="kepek\logo.png" alt="kep" />
+                  <img src="kepek\first.png" alt="kep" />
+                </div>
+                  <div className="oszlop2">
+                    <div className="kep2">
+                    <img src="kepek/second.png" alt="kep" />
+                  </div>
+                  <div className="kep3">
+                     <img src="kepek/third.png" alt="kep" />
+                  </div>
+                  </div> 
+                </div>
+                
+                <div className="tomb">
+                  <div className="kep4">
+                    Emelet 
+                  </div>
+                  <div className="kep4">
+                    Emelet 
+                  </div>
+                  <div className="kep4">
+                    Emelet 
+                  </div>
+                  <div className="kep4">
+                    Emelet 
+                  </div>
+                  <div className="kep4">
+                    Emelet 
+                  </div>
+                  <div className="kep4">
+                    Emelet 
+                  </div>
+                  <div className="kep4">
+                    Emelet 
+                  </div>
+                  <div className="kep4">
+                    Emelet 
+                  </div>
+                  <div className="kep4">
+                    Emelet 
+                  </div>
+                  <div className="kep4">
+                    Emelet 
+                  </div>
+                  <div className="kep4">
+                    Emelet 
+                  </div>
+                  <div className="kep4">
+                    Emelet 
+                  </div>
+                  <div className="kep4">
+                    Emelet 
+                  </div>
+                  <div className="kep4">
+                    Emelet 
+                  </div>
+                  <div className="kep4">
+                    Emelet 
+                  </div>
+                  <div className="kep4">
+                    Emelet 
+                  </div>
+                
+                </div>
+                
+                
+                
+                
+                </div>
+        
+
+
+  </div>
+
+            {/* <p>
+                Connecting Brands Across Central Europe Full-service creative and production with studios in Budapest, Prague and Bratislava, serving the CE region
+            </p> */}
+            </article>
+    </div>
+  );
+};
+
+export default Oldal;
