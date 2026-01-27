a7c6ee32bef4d568e9dfed5dcc746a4bb5b30462
DDorka02 <dobszaydorka@gmail.com>
2026-01-27 08:32:15 +0100

    majnem kész állapot

--- src/componens/Oldal.js
+++ src/componens/Oldal.js
@@ -3,88 +3,88 @@ import "./Oldal.css";
 const Oldal = () => {
   return (
     <div className="App">
-       <article className="article">
-            <div className="emelet">
-              <div className="kepek">
-                <div className="oszlop">
-                <div className="kep1">
-                  <img src="kepek\logo.png" alt="kep" />
-                  <img src="kepek\first.png" alt="kep" />
+       <article className="main">
+            <div className="container">
+                <div className="left">
+                  {/* <img src="kepek\logo.png" alt="kep" /> */}
+                  <img src="kepek\first.png" width="700" height="500" alt="kep" />
                 </div>
-                  <div className="oszlop2">
-                    <div className="kep2">
-                    <img src="kepek/second.png" alt="kep" />
-                  </div>
-                  <div className="kep3">
-                     <img src="kepek/third.png" alt="kep" />
-                  </div>
+                  <div className="right">
+                    <div className="action-btn yellow">
+                      <img src="kepek\second.png" width="700" height="500" alt="kep" />
+                    </div>
+                    <div className="action-btn yellow">
+                      <img src="kepek\third.png" width="700" height="500" alt="kep" />
+                    </div>
                   </div> 
-                </div>
-                
-                <div className="tomb">
-                  <div className="kep4">
-                    Emelet 
+          </div>      
+                <div className="container2">
+                  <div className="oszlop1">
+                    <img src="kepek\create.png" width="590" height="1195" alt="kep" />
+                    <img src="kepek\merch.png" width="450" height="579" alt="kep" />
+                    <img src="kepek\reserve.png" width="300" height="595" alt="kep" /> 
+                    <img src="kepek\string.png" width="1450" height="186" alt="kep" /> 
                   </div>
-                  <div className="kep4">
-                    Emelet 
+                  <div className="oszlop2">
+                    <img src="kepek\print.png" width="450" height="590" alt="kep" />
+                    <img src="kepek\newspaper.png" width="450" height="592" alt="kep" />
+                    <img src="kepek\drink.png" width="450" height="579" alt="kep" />
+                    <img src="kepek\smink.png" width="300" height="595" alt="kep" />   
                   </div>
-                  <div className="kep4">
-                    Emelet 
+                  <div className="oszlop3">
+                    <img src="kepek\tecnology.png" width="450" height="590" alt="kep" /> 
+                    <img src="kepek\phone2.png" width="450" height="592" alt="kep" />
+                    <img src="kepek\buzz.png" width="700" height="1189" alt="kep" />
                   </div>
-                  <div className="kep4">
-                    Emelet 
+                  <div className="oszlop4">
+                     <img src="kepek\phone.png" width="450" height="590" alt="kep" /> 
+                     <img src="kepek\house.png" width="700" height="1175" alt="kep" /> 
+                    <img src="kepek\cow.png" width="450" height="833" alt="kep" /> 
                   </div>
-                  <div className="kep4">
-                    Emelet 
                   </div>
-                  <div className="kep4">
-                    Emelet 
+                  <div className="container3">
+                    <div className="oszlop1">
+                    <img src="kepek\cash.png" width="5920" height="586" alt="kep" />
+                    <img src="kepek\dog.png" width="450" height="938" alt="kep" />
                   </div>
-                  <div className="kep4">
-                    Emelet 
+                  <div className="oszlop2">
+                    <img src="kepek\lelosi.png" width="450" height="584" alt="kep" />
+                    <img src="kepek\breakfast.png" width="1200" height="940" alt="kep" />  
                   </div>
-                  <div className="kep4">
-                    Emelet 
+                  <div className="oszlop3">
+                    <img src="kepek\layers.png" width="550" height="584" alt="kep" /> 
+                    <img src="kepek\inhouse.png" width="624" height="977" alt="kep" />
                   </div>
-                  <div className="kep4">
-                    Emelet 
                   </div>
-                  <div className="kep4">
-                    Emelet 
+                  <div className="container4">
+                  <div className="oszlop1">
+                    <img src="kepek\ce.png" width="450" height="1052" alt="kep" />
                   </div>
-                  <div className="kep4">
-                    Emelet 
+                  <div className="oszlop2">
+                    <img src="kepek\isana.png" width="450" height="652" alt="kep" />
+                    <img src="kepek\trusted.png" width="450" height="250" alt="kep" />
                   </div>
-                  <div className="kep4">
-                    Emelet 
+                  <div className="oszlop3">
+                    <img src="kepek\stada.png" width="450" height="652" alt="kep" /></div>
+                  <div className="oszlop4">
+                    <img src="kepek\multi.png" width="450" height="1052" alt="kep" /> 
                   </div>
-                  <div className="kep4">
-                    Emelet 
                   </div>
-                  <div className="kep4">
-                    Emelet 
-                  </div>
-                  <div className="kep4">
-                    Emelet 
-                  </div>
-                  <div className="kep4">
-                    Emelet 
-                  </div>
-                
-                </div>
-                
-                
-                
-                
-                </div>
-        
 
-
-  </div>
-
-            {/* <p>
-                Connecting Brands Across Central Europe Full-service creative and production with studios in Budapest, Prague and Bratislava, serving the CE region
-            </p> */}
+                  <div className="container5">
+                  <div className="oszlop1">
+                    <img src="kepek\map.png" width="450" height="1073" alt="kep" /> 
+                  </div>
+                  <div className="oszlop2">
+                    <img src="kepek\hammer.png" width="450" height="1073" alt="kep" /> 
+                  </div>
+                    </div>
+                  <div className="container6">
+                    <div className="oszlop1">
+                    <img src="kepek\finish.png" width="2448" height="611" alt="kep" /> 
+                  </div>
+                  
+                </div>
             </article>
     </div>
   );
